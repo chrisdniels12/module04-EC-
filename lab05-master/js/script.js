36f358580418e2aad55fc22a50d175f5d64b078a
Chris <chrisdniels12@gmail.com>
2024-03-06 14:26:33 -0800

    Update script.js
    add while loop for input validation

--- lab05-master/js/script.js
+++ lab05-master/js/script.js
@@ -13,5 +13,10 @@ do {
         alert('One or both entries are invalid.');
     }
     again = prompt('Run application again? (y or n)', 'y');
+    //  iterate while both conditions are true, if y or n is entered, one of the conditions is false and exit the loop
+    // if user enters anything other than y or n, the loop will continue 
+    while (again !== "y" && again !== "n") {
+        again = prompt('Run application again? (y or n)', 'y')
+    }
 } while (again === 'y');
 console.log('Application has exited.');
